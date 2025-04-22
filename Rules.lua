5e8876677baf30209ec3259bbe2ee636aa0bd261
Jan Heise <jan@heise.rocks>
2025-04-22 17:19:55 +0200

    Prohibit Auctionhouse Usage
    closes #9

--- Rules.lua
+++ Rules.lua
@@ -7,14 +7,23 @@ function SchlingelInc.Rules.ProhibitMailboxUsage()
     CloseMail() -- Schließt den Briefkasten
 end
 
+-- Regel: Auktionshaus-Nutzung verbieten
+function SchlingelInc.Rules.ProhibitAuctionhouseUsage()
+    SchlingelInc:Print("Die Nutzung des Auktionshauses ist verboten!")
+    CloseAuctionHouse() -- Schließt das Auktionshaus
+end
+
 -- Initialisierung der Regeln
 function SchlingelInc.Rules:Initialize()
     local frame = CreateFrame("Frame")
-    frame:RegisterEvent("MAIL_SHOW") -- Event für Briefkasten öffnen
+    frame:RegisterEvent("MAIL_SHOW")          -- Event für Briefkasten öffnen
+    frame:RegisterEvent("AUCTION_HOUSE_SHOW") -- Event für Auktionshaus öffnen
 
     frame:SetScript("OnEvent", function(_, event)
         if event == "MAIL_SHOW" then
             SchlingelInc.Rules.ProhibitMailboxUsage()
+        elseif event == "AUCTION_HOUSE_SHOW" then
+            SchlingelInc.Rules.ProhibitAuctionhouseUsage()
         end
     end)
 end
