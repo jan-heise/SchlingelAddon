1ff9815125090c680763fe5146cf9ddbef696b68
Pudl0 <patrick.pudlo92@gmail.com>
2025-04-29 15:58:58 +0200

    adding stuff from old addon and removing everything not needed. Now needs to be tested and finetuned

--- Death.lua
+++ Death.lua
@@ -8,8 +8,46 @@ DeathFrame:RegisterEvent("PLAYER_DEAD")
 -- Event-Handler
 DeathFrame:SetScript("OnEvent", function(self, event, ...)
     if event == "PLAYER_DEAD" then
-        CharacterDeaths = CharacterDeaths + 1
-        SchlingelInc:Print("Du bist gestorben. Tode insgesamt: " .. CharacterDeaths)
+        
+	-- Vorbereiten der genutzten Variablen für die GildenNachricht
+	local name = UnitName("player")
+	local _, rank = GetGuildInfo("player")
+	local _, _, classID = UnitClass("player")
+	local class = CLASSES[classID]
+	local level = UnitLevel("player")
+	local zone, mapID
+	if IsInInstance() then
+		zone = GetInstanceInfo()
+	else
+		mapID = C_Map.GetBestMapForUnit("player")
+		zone = C_Map.GetMapInfo(mapID).name
+	end
+
+	-- Formatiert die Broadcast Nachricht
+    local messageFormat = "%s ist mit Level %d in %s gestorben. Schande!"
+	local messageFormatWithRank = "Ewiger Schlingel %s ist mit Level %d in %s gestorben. Schande!"
+	if (rank ~= nil and rank == "EwigerSchlingel") then
+		messageFormat = messageFormatWithRank
+	end
+	local messageString = messageFormat:format(name, level, zone)
+	if not (Last_Attack_Source == nil) then
+		messageString = string.format("%s Gestorben an %s", messageString, Last_Attack_Source)
+		Last_Attack_Source = nil
+	end
+
+	-- Letzte Worte
+	if not (recent_msg["text"] == nil) then
+		messageString = string.format('%s. Die letzten Worte: "%s"', messageString, recent_msg["text"])
+	end
+
+	-- -- Send broadcast text messages to guild and greenwall
+	-- selfDeathAlert(DeathLog_Last_Attack_Source)
+	-- selfDeathAlertLastWords(recent_msg["text"])
+
+	-- SendChatMessage(messageString, "GUILD")
+
+    CharacterDeaths = CharacterDeaths + 1
+	SchlingelInc:Print(messageString)
     end
 end)
 
@@ -18,7 +56,7 @@ SLASH_DEATHSET1 = '/deathset'
 SlashCmdList["DEATHSET"] = function(msg)
     local inputValue = tonumber(msg)
 
-    -- Kommt keine Zahl vom User, git es eine Fehlermeldung plus Anleitung.
+    -- Kommt keine Zahl vom User, gibt es eine Fehlermeldung plus Anleitung.
     if not inputValue then
         SchlingelInc:Print("Ungültiger Input. Benutze: /deathset <Zahl>")
         return
