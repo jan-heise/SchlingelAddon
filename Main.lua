8dedfc63ac714c70c76364e276a41e3ecb44fe8f
Jan Heise <jan@heise.rocks>
2025-05-06 17:40:03 +0200

    Remove Greenwall from deps but enable check if it is enabled

--- Main.lua
+++ Main.lua
@@ -6,6 +6,14 @@ StaticPopupDialogs["SCHLINGEL_HARDCOREUNLOCKED_WARNING"] = {
     hideOnEscape = true,
     preferredIndex = 3,
 }
+StaticPopupDialogs["SCHLINGEL_GREENWALL_MISSING"] = {
+    text = "Du hast Greenwall nicht aktiv.\nBitte aktiviere oder installiere es!",
+    button1 = "OK",
+    timeout = 0,
+    whileDead = true,
+    hideOnEscape = true,
+    preferredIndex = 3,
+}
 
 -- Initialisierung des Addons
 function SchlingelInc:OnLoad()
@@ -20,19 +28,31 @@ function SchlingelInc:OnLoad()
     SchlingelInc:InitMinimapIcon()
     -- Prüfung auf Hardcore Unlocked bzw Schlingel Addon
     local numAddons = GetNumAddOns()
+    local greenwall_found = false
     for i = 1, numAddons do
         local name, _, _, enabled = GetAddOnInfo(i)
         if name == "HardcoreUnlocked" or "SchlingelAddon" and enabled == 1 then
             SchlingelInc:Print(
-            "|cffff0000Warnung: Du hast das veraltete Addon aktiv. Bitte entferne es, da es zu Problemen mit SchlingelInc führt!|r")
+                "|cffff0000Warnung: Du hast das veraltete Addon aktiv. Bitte entferne es, da es zu Problemen mit SchlingelInc führt!|r")
             StaticPopup_Show("SCHLINGEL_HARDCOREUNLOCKED_WARNING")
             break
         end
+
+        if name == "Greenwall" and enabled == 1 then
+            greenwall_found = true
+        end
     end
+    if greenwall_found == false then
+        SchlingelInc:Print(
+            "|cffff0000Warnung: Du hast Greenwall nicht aktiv. Bitte aktiviere oder installiere es!|r")
+        StaticPopup_Show("SCHLINGEL_GREENWALL_MISSING")
+    end
+
+
     SchlingelInc:Print("Addon version " .. SchlingelInc.version .. " geladen")
     if CharacterDeaths == nil then
         SchlingelInc:Print(
-        "Keine Tode gefunden.\nBitte initialisiere deinen DeathCounter einmal mit /deathset <Zahl>. Danke für deine Ehrlichkeit! :)")
+            "Keine Tode gefunden.\nBitte initialisiere deinen DeathCounter einmal mit /deathset <Zahl>. Danke für deine Ehrlichkeit! :)")
     end
 end
 
